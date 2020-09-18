2438a796db890119ec44790eb5b3eb531ee574cb
daniel-castrillo <daniel.castrillo@gamesys.co.uk>
2020-09-18 12:45:21 +0100

    python script to run automatic notifications

---
+++ notify.py
@@ -0,0 +1,15 @@
+import os
+
+import subprocess
+
+def notify(notification):
+	title = 'Mangas updated'
+	action = f'display notification "{notification}" with title "{title}"'
+	script = f'osascript -e \'{action}\''
+	print(notification)
+	os.system(script)
+
+(response, err) = subprocess.Popen(["mangas -n"], stdout=subprocess.PIPE, shell=True).communicate()
+
+
+notify(response.decode("utf-8"))
